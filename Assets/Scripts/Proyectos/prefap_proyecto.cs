d4b0e81b815d3893f336d38a2a756de4319adb4b
alex25cvb <cruballesalex2506@gmail.com>
2025-11-20 16:23:17 -0600

    Verificar ultima vez modificado
    Se subio el comportamiento de cuanto tiempo paso de la ultima interaccion de un proyecto

--- Assets/Scripts/Proyectos/prefap_proyecto.cs
+++ Assets/Scripts/Proyectos/prefap_proyecto.cs
@@ -38,6 +38,9 @@ public class prefap_proyecto : MonoBehaviour, IPointerClickHandler, IPointerEnte
         if (fondo != null)
             fondo.color = normalColor;
         VerificarFavorito();
+
+        ultimaModificacionProyecto.text = VerificarTiempoPasado();
+
         botonFavorito.onClick.AddListener(() =>
         {
             try
@@ -52,6 +55,7 @@ public class prefap_proyecto : MonoBehaviour, IPointerClickHandler, IPointerEnte
                     botonFavorito.image.sprite = iconoFavoritoOff;
                     QuitarFavorito();
                 }
+                
             }
             catch (Exception ex)
             {
@@ -86,6 +90,7 @@ public class prefap_proyecto : MonoBehaviour, IPointerClickHandler, IPointerEnte
     private void OnClick()
     {
         Debug.Log($"Proyecto seleccionado: {idProyecto} - {nombreProyecto.text}");
+        UltimaVezInteract();
     }
 
     #region Eventos PunterosMouse
@@ -125,6 +130,36 @@ public class prefap_proyecto : MonoBehaviour, IPointerClickHandler, IPointerEnte
             Debug.LogError($"Error al verificar favorito: {ex.Message}");
             botonFavorito.image.sprite = iconoFavoritoOff;
         }
+    }
+
+    private void UltimaVezInteract()
+    {
+        PlayerPrefs.SetString($"UltimaInteraccion_{idProyecto}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+        
+    }
+
+    private string VerificarTiempoPasado()
+    {
+        string ultimaInteraccion = PlayerPrefs.GetString($"UltimaInteraccion_{idProyecto}", "");
+        string tiempoPasadoStr = "";
+        if (!string.IsNullOrEmpty(ultimaInteraccion))
+        {
+            DateTime ultimaFecha = DateTime.Parse(ultimaInteraccion);
+            TimeSpan tiempoPasado = DateTime.Now - ultimaFecha;
+            tiempoPasadoStr = $"{tiempoPasado.TotalMinutes} minutos";
+            if(tiempoPasado.TotalSeconds < 60)
+                tiempoPasadoStr = "Hace unos segundos";
+            else if(tiempoPasado.TotalMinutes < 60)
+                tiempoPasadoStr = $"Hace {Math.Floor(tiempoPasado.TotalMinutes)} minutos";
+            else if(tiempoPasado.TotalHours < 24)
+                tiempoPasadoStr = $"Hace {Math.Floor(tiempoPasado.TotalHours)} horas";
+            else
+                tiempoPasadoStr = $"Hace {Math.Floor(tiempoPasado.TotalDays)} días";
+
+
+            Debug.Log($"Tiempo pasado desde la última interacción con el proyecto {idProyecto}: {tiempoPasado.TotalMinutes} minutos.");
+        }
+        return tiempoPasadoStr;
     }
 
 
