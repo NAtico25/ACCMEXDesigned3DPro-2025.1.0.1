cb3d9de1220c51e13028d4a8690fa73cff393d1b
alex25cvb <cruballesalex2506@gmail.com>
2025-11-20 12:05:19 -0600

    eventodlobleclick
    se agrego el evento dobleclick

--- Assets/Scripts/Proyectos/prefap_proyecto.cs
+++ Assets/Scripts/Proyectos/prefap_proyecto.cs
@@ -3,14 +3,18 @@ using System.Data;
 using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class prefap_proyecto : MonoBehaviour
+public class prefap_proyecto : MonoBehaviour, IPointerClickHandler
 {
     public CanvasRenderer favoritoProyecto;
     public TextMeshProUGUI nombreProyecto;
     public TextMeshProUGUI ultimaModificacionProyecto;
     public TextMeshProUGUI fechaProyecto;
     public int idProyecto;
+
+    private float lastClickTime = 0f;
+    private const float doubleClickThreshold = 0.25f;
     // Start is called before the first frame update
 
     public void SetData(int id, string nombre, string fecha)
@@ -30,4 +34,25 @@ public class prefap_proyecto : MonoBehaviour
     {
         
     }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+
+        float timeSinceLastClick = Time.time - lastClickTime;
+
+        if (timeSinceLastClick <= doubleClickThreshold)
+        {
+            // Doble clic detectado
+            Debug.Log($"Doble clic en proyecto: {idProyecto} - {nombreProyecto.text}");
+            OnClick();
+        }
+
+        lastClickTime = Time.time;
+    }
+
+    private void OnClick()
+    {
+        Debug.Log($"Proyecto seleccionado: {idProyecto} - {nombreProyecto.text}");
+    }
+
 }
