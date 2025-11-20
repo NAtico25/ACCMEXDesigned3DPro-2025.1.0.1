cb46d42802e5d2f0f8054bb17a6585ed090a6613
alex25cvb <cruballesalex2506@gmail.com>
2025-11-20 15:12:09 -0600

    evento click prefap

--- Assets/Scripts/Proyectos/prefap_proyecto.cs
+++ Assets/Scripts/Proyectos/prefap_proyecto.cs
@@ -4,14 +4,19 @@ using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class prefap_proyecto : MonoBehaviour, IPointerClickHandler
+public class prefap_proyecto : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public CanvasRenderer favoritoProyecto;
     public TextMeshProUGUI nombreProyecto;
     public TextMeshProUGUI ultimaModificacionProyecto;
     public TextMeshProUGUI fechaProyecto;
     public int idProyecto;
+
+    public Image fondo;      // el Image que creaste como Background
+    public Color normalColor = new Color(1, 1, 1, 0);      // transparente
+    public Color hoverColor = new Color(0.8f, 0.8f, 0.8f, 0.3f);  // gris claro
 
     private float lastClickTime = 0f;
     private const float doubleClickThreshold = 0.25f;
@@ -26,7 +31,8 @@ public class prefap_proyecto : MonoBehaviour, IPointerClickHandler
     }
     void Start()
     {
-        
+        if (fondo != null)
+            fondo.color = normalColor;
     }
 
     // Update is called once per frame
@@ -53,6 +59,19 @@ public class prefap_proyecto : MonoBehaviour, IPointerClickHandler
     private void OnClick()
     {
         Debug.Log($"Proyecto seleccionado: {idProyecto} - {nombreProyecto.text}");
+    }
+
+    
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        fondo.color = hoverColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        fondo.color = normalColor;
     }
 
 }
