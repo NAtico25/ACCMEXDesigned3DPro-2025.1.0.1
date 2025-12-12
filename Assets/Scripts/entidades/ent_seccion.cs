d0a1808750975417ecaf8b0fec18f54a8c683461
alex25cvb <cruballesalex2506@gmail.com>
2025-12-12 12:35:17 -0600

    Update ent_seccion.cs

--- Assets/Scripts/entidades/ent_seccion.cs
+++ Assets/Scripts/entidades/ent_seccion.cs
@@ -24,26 +24,6 @@ public class ent_seccion : MonoBehaviour
     public Mat_angulo_anclaje angulos_anclaje { get; set; } // son las L
     public Mat_conector_sugestion conectores_sujecion { get; set; }
 
-    //public string nombre_Seccion = "Seccion";
-    //public int no_seccion;
-    //public Mat_zoclo zoclo;
-    //public Mat_piezas_anclaje piezas_Anclaje;
-    //public Mat_orejas_carga orejas_Carga;
-    //public Mat_placas_metal_piso placas_Metal_Piso;
-    //public List<Mat_ang_piso> angulos_piso;
-    //public List<Mat_ang_techo> angulos_techo;
-    //public List<Mat_perfil> perfiles; // largo
-    //public List<Mat_piso.piso> pisos; // Si son para silleta o lisos (enum)
-    //public Mat_cubre_bus cubre_Bus;
-    //public Mat_porta_bus porta_Bus;
-    //public Mat_bus_lateral bus_lateral;
-    //public List<Mat_tapa_bus_lateral> tapas_bus_lateral; // largo y ancho
-    //public Mat_conector_bus conectores_bus;
-    //public List<Mat_puerta> puertas; // cada una debe tener sus medidas (alto y ancho) cuando se agrega se debe agregar un numero de seguros y porta seguros (esto lo define el usuario)
-    //public Mat_pestana_superior pestana_superior; // la cosa que cubre la etiqueta roja de arriba
-    //public Mat_angulo_anclaje angulos_anclaje; // son las L
-    //public Mat_conector_sugestion conectores_sujecion;
-
 
     // cuando se agrega una silleta se debe agregar piso, porta clemas, clemas, guia de silleta, carretillas, acrilicos separadores, clemas de fuerza.
 
