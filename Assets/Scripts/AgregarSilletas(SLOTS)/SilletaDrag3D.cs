8287638db4be430fdd4ad89ee72ac81e3909999c
ACCMEX <accmex84@gmail.com>
2026-01-22 12:57:07 -0600

    Diseño de Botones al 50%
    Se crearon los diseños que se utilizaran para las silletas, en el cual podremos ver imagenes de las silletas y sepondran en los slots

--- Assets/Scripts/AgregarSilletas(SLOTS)/SilletaDrag3D.cs
+++ Assets/Scripts/AgregarSilletas(SLOTS)/SilletaDrag3D.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class SilletaDrag3D : MonoBehaviour
+public class SilletaDrag3D : MonoBehaviour, 
+    IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     private Camera cam;
     private SilletaInstanciada silleta;
     private Vector3 offset;
     private AgregarSilletas manager;
+
+    private Plane dragPlane;
+
 
     void Awake()
     {
@@ -26,26 +31,43 @@ public class SilletaDrag3D : MonoBehaviour
             offset = transform.position - ray.GetPoint(d);
     }
 
-    void OnMouseDrag()
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        Plane p = new Plane(Vector3.up, transform.position);
-        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-        if (p.Raycast(ray, out float d))
-            transform.position = ray.GetPoint(d) + offset;
+        // Liberar slots al comenzar a mover
+        LiberarSlots();
+
+        dragPlane = new Plane(Vector3.up, transform.position);
+        Ray ray = cam.ScreenPointToRay(eventData.position);
+
+        if (dragPlane.Raycast(ray, out float d))
+        {
+            offset = transform.position - ray.GetPoint(d);
+        }
     }
 
-    void OnMouseUp()
+    public void OnDrag(PointerEventData eventData)
     {
-        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        Ray ray = cam.ScreenPointToRay(eventData.position);
+
+        if (dragPlane.Raycast(ray, out float d))
+        {
+            transform.position = ray.GetPoint(d) + offset;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Ray ray = cam.ScreenPointToRay(eventData.position);
 
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
             SlotsDrop3D slot = hit.collider.GetComponent<SlotsDrop3D>();
+
             if (slot && manager.TryPlaceSilleta(silleta, slot.slotIndex))
                 return;
         }
 
-        // no válido regresar
+        //No válido  regresar
         transform.position = silleta.lastValidPosition;
         ReocuparSlots();
     }
