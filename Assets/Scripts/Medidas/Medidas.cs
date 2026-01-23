9b90ec564e3359b91a08e41284d88d384cc0fd91
ACCMEX <accmex84@gmail.com>
2026-01-23 12:47:02 -0600

    Medidas
    Se creo el script de medidas y ya aparecen los numero de cuanto mide la seccion, falta el inspector

---
+++ Assets/Scripts/Medidas/Medidas.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using UnityEngine;
+
+public class Medidas : MonoBehaviour
+{
+    [Header("Referencia")]
+    public ent_seccion seccion;
+
+    [Header("UI")]
+    public TMP_Text textoLargo;
+    public TMP_Text textoAncho;
+
+    void Start()
+    {
+        ActualizarTexto();
+    }
+
+    public void ActualizarTexto()
+    {
+        if (seccion == null || textoLargo == null) return;
+
+        textoLargo.text = $"{seccion.largo_Seccion:F2} m";
+        textoAncho.text = $"{seccion.ancho_Seccion:F2} cm";
+    }
+}
